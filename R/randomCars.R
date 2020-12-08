5ebe80d4afac0cde639e52d62874e01ee91689da
Phillip Wong <pwong@wesleyan.edu>
2020-12-08 16:13:14 -0500

    add text, change developers, add data set

---
+++ R/randomCars.R
@@ -0,0 +1,19 @@
+#' 99 Values in mtcars dataset
+#'
+#' Using the mtcars dataset, and plugging in 100 99 values randomly throughout the data set.
+#'
+#' @format A data frame with 32 rows and 11 variables:
+#' \describe{
+#'   \item{\code{mpg}}{double. miles per gallon.}
+#'   \item{\code{cyl}}{double. cylinder of car.}
+#'   \item{\code{disp}}{double. displacement.}
+#'   \item{\code{hp}}{double. gross horsepower.}
+#'   \item{\code{drat}}{double. rear axle ratio.}
+#'   \item{\code{wt}}{double. weight over 1000.}
+#'   \item{\code{qsec}}{double. 1/4 mile time.}
+#'   \item{\code{vs}}{double. variable signaling.}
+#'   \item{\code{am}}{double. transmission type.}
+#'   \item{\code{gear}}{double. number of forward gears.}
+#'   \item{\code{carb}}{double. number of carburetors.}
+#' }
+"randomCars"
